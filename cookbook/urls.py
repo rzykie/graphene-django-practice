267f603c150125752bcf5e57c03f67d4821943b2
Zykie Rallos <zykie1018@gmail.com>
2023-05-13 18:26:16 +0800

    Update: add schema parameter in graphql url path

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -19,10 +19,12 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
+from cookbook.schema import schema
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
         "graphql",
-        csrf_exempt(GraphQLView.as_view(graphiql=True)),
+        csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema)),
     ),
 ]
