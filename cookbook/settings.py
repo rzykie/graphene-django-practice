4b6d000d51d0ff4a59953af6802afc67e93b6ef3
Zykie Rallos <zykie1018@gmail.com>
2023-05-13 18:22:59 +0800

    Update: add schema to graphene config in settings

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -129,3 +129,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+GRAPHENE = {
+    "SCHEMA": "cookbook.schema.schema",
+}
